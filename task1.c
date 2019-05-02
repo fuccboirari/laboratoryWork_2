33dd0d6b835c6af0c5ab1a155696a67bbe287a3f
fuccboirari <47291736+fuccboirari@users.noreply.github.com>
2019-05-02 11:14:04 +0300

    main

---
+++ task1.c
@@ -0,0 +1,10 @@
+﻿//Var 17, t.k. vsego 15 ==> 2
+
+#include <math.h>
+
+_Bool isInArea(double x, double y) {
+	if (((x >= -1 && x <= 0) && (y <= 1 && y >= 0) && (x * x + y * y <= 1)) || ((x >= 0 && x <= 1) && (y >= -1 && y <= 0) && (pow(x, 2) + pow(y, 2) == 1)))
+		return 1;
+	else
+		return 0;
+}
---
+++ task2.c
@@ -0,0 +1,6 @@
+﻿#include <math.h>
+
+double f(double x) {
+		x >= 1.1 ? 9 - x : sin(3 * x) / pow(x, 4) + 1;
+		return;
+	}
